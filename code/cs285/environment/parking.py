6a721df80e5e8917af7597dbf8766b29f8dc0fcb
YunduanLin <linyunduan0805@gmail.com>
2020-12-13 11:59:31 -0800

    deal with parked vehicles

--- code/cs285/environment/parking.py
+++ code/cs285/environment/parking.py
@@ -7,17 +7,28 @@ class parking_block():
         # params are from csv file
         # dist records the distance between each two blocks
 
-        self.id = params['BLOCKFACE_ID']
+        self.block_id = params['BLOCKFACE_ID']
         self.loc = (params['LONGITUDE'], params['LATITUDE'])
-        self.cap = params['SPACE_NUM']
+        self.capacity = params['SPACE_NUM']
         self.occupied = 0   # the count of occupied meters
         self.backup_block = np.argsort(dist)    # the priority of back-up blocks
+
+class vehicle():
+    def __init__(self, params):
+        self.loc_arrive = params['id']
+        self.loc_current = params['id']
+        self.parked = False
+        self.remaining_time = 0
+
+    def decrease_1(self):
+        self.remaining_time -= 1
 
 class parking_env():
     def __init__(self, df):
         self.t = 0
         mat_distance = self.great_circle_v(df['LONGITUDE'].values, df['LATITUDE'].values)
         self.blocks = [parking_block(record, mat_distance[i]) for i, record in enumerate(df.to_dict('records'))]
+        self.vehicles = []
 
     # calculate the great circle distance for all the blocks with matrix form
     def great_circle_v(self, lon, lat):
@@ -30,11 +41,24 @@ class parking_env():
 
     # simulate the parking behavior with choice model
     def do_simulation(self):
+        # parked vehicles
+        ind_vehicles = []
+        for i, vehicle in enumerate(self.vehicles):
+            vehicle.decrease_1()
+            if vehicle.remaining_time == 0:
+                self.blocks[vehicle.loc_current].occupied -= 1
+            else:
+                ind_vehicles.append(i)
+        self.vehicles = self.vehicles[ind_vehicles]
+
+        # parking vehicles
+
         return
 
     # given the action, simulate the process and get the reward
     def step(self, a):
         d = self.generate_demand()
+        self.do_simulation()
         for i, block in enumerate(self.blocks):
             for u in range(d[i]):
                 self.do_simulation()
