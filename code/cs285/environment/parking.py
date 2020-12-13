980809cacc11fd360840df712b96929b7f9d858b
YunduanLin <linyunduan0805@gmail.com>
2020-12-12 17:12:27 -0800

    calculate great circle distance

--- code/cs285/environment/parking.py
+++ code/cs285/environment/parking.py
@@ -3,21 +3,22 @@ import numpy as np
 EARTH_D = 6371
 
 class parking_block():
-    def __init__(self, id, lon, lat, cap, params):
-        self.id = id
-        self.loc = (lon, lat)
-        self.cap = cap
+    def __init__(self, params):
+        self.id = params['BLOCKFACE_ID']
+        self.loc = (params['LONGITUDE'], params['LATITUDE'])
+        self.cap = params['SPACE_NUM']
         self.occupied = 0
 
 class parking_env():
     def __init__(self, df):
         self.t = 0
-        self.mat_distance = self.great_circle_v(df['LONGITUDE'].values, df['LATITUDE'].values)
-        self.blocks = parking_block()
+        mat_distance = self.great_circle_v(df['LONGITUDE'].values, df['LATITUDE'].values)
+        self.blocks = [parking_block(record) for record in df.to_dict('records')]
 
     def great_circle_v(self, lon, lat):
-        return EARTH_D * np.acos(np.sin(lat) * np.sin(lat).reshape(-1, 1) \
-                + np.cos(lon) * np.cos(lon).reshape(-1, 1) * np.cos(lon-lon.reshape(-1, 1)))
+        lon, lat = np.radians(lon), np.radians(lat)
+        return EARTH_D * np.arccos(np.sin(lat) * np.sin(lat).reshape(-1, 1) \
+                + np.cos(lat) * np.cos(lat).reshape(-1, 1) * np.cos(lon-lon.reshape(-1, 1)))
 
     def step(self, a):
         return
