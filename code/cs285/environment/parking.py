1288787d33defc536d82146681c36693a0030927
YunduanLin <linyunduan0805@gmail.com>
2020-12-12 16:38:21 -0800

    construct parking environment

--- code/cs285/environment/parking.py
+++ code/cs285/environment/parking.py
@@ -1,11 +1,26 @@
 import numpy as np
 
+EARTH_D = 6371
+
+class parking_block():
+    def __init__(self, id, lon, lat, cap, params):
+        self.id = id
+        self.loc = (lon, lat)
+        self.cap = cap
+        self.occupied = 0
+
 class parking_env():
-    def __init__(self):
-        pass
+    def __init__(self, df):
+        self.t = 0
+        self.mat_distance = self.great_circle_v(df['LONGITUDE'].values, df['LATITUDE'].values)
+        self.blocks = parking_block()
+
+    def great_circle_v(self, lon, lat):
+        return EARTH_D * np.acos(np.sin(lat) * np.sin(lat).reshape(-1, 1) \
+                + np.cos(lon) * np.cos(lon).reshape(-1, 1) * np.cos(lon-lon.reshape(-1, 1)))
 
     def step(self, a):
-        pass
+        return
 
     def _get_obs(self):
         pass
--- code/cs285/infrastructure/utils.py
+++ code/cs285/infrastructure/utils.py
@@ -63,16 +63,16 @@ def sample_trajectory(env, policy, max_path_length, render=False, render_mode=('
     steps = 0
     while True:
 
-        # render image of the simulated env
-        if render:
-            if 'rgb_array' in render_mode:
-                if hasattr(env, 'sim'):
-                    image_obs.append(env.sim.render(camera_name='track', height=500, width=500)[::-1])
-                else:
-                    image_obs.append(env.render(mode=render_mode))
-            if 'human' in render_mode:
-                env.render(mode=render_mode)
-                time.sleep(env.model.opt.timestep)
+        # # render image of the simulated env
+        # if render:
+        #     if 'rgb_array' in render_mode:
+        #         if hasattr(env, 'sim'):
+        #             image_obs.append(env.sim.render(camera_name='track', height=500, width=500)[::-1])
+        #         else:
+        #             image_obs.append(env.render(mode=render_mode))
+        #     if 'human' in render_mode:
+        #         env.render(mode=render_mode)
+        #         time.sleep(env.model.opt.timestep)
 
         # use the most recent ob to decide what to do
         obs.append(ob)
